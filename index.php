d7cc341d2a48ba2dfeffe6208d3d09c2b79c97a6
Patricia dos Santos Nascimento <englishwithpatricia007@gmail.com>
2026-05-16 11:24:41 -0300

    Primeiro commit: adicionando arquivos do projeto

---
+++ index.php
@@ -0,0 +1,13 @@
+<?php
+
+$controller = 'index';
+
+if (isset($_SERVER['PATH_INFO'])) {
+
+    $controller = str_replace('/', '', $_SERVER['PATH_INFO']);
+
+}
+
+require "controllers/{$controller}.controller.php";
+
+?>
